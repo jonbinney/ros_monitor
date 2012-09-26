b5d7f6aca167d08b28211ad5f04e2c5f04a2114a
Jonathan Binney <jbinney@bih.willowgarage.com>
2012-09-26 11:58:13 -0700

    added messages for usage data

--- src/ros_monitor/node_info.py
+++ src/ros_monitor/node_info.py
@@ -5,9 +5,10 @@ class NodeInfo:
     Exposes information about the ROS nodes which are running.
     '''
 
-    pid_re = re.compile('.+Pid:\s+([0-9]+)', re.DOT_ALL, re.MULTILINE)
+    pid_re = re.compile('Pid:\s+([0-9]+)', re.DOTALL|re.MULTILINE|re.IGNORECASE)
+    topic_re = re.compile('topic:\s+(\S+)', re.DOTALL|re.MULTILINE|re.IGNORECASE)
+    topic_to_re = re.compile('to:\s+(\S+)\s+\((\S+)\)', re.DOTALL|re.MULTILINE|re.IGNORECASE)
 
-    
     def __init__(self):
         pass
 
@@ -15,19 +16,44 @@ class NodeInfo:
         return subprocess.check_output(['rosnode', 'list']).strip().split('\n')
 
     def get_node_info(self, node_name):
+        '''
+        Uses command "rosnode info <node_name>" to get pid and connection info for this node.
+
+        Parsing the command line output for this info seems a bit hacky, but I\'d rather
+        not rely on the internal python API. -jbinney
+        '''
         info_str = subprocess.check_output(['rosnode', 'info', node_name])
-        m = pid_re.search(info_str)
+        info_dict = {}
+        
+        m = NodeInfo.pid_re.search(info_str)
         if m:
-            return {'pid': m.groups()[0]}
+            info_dict['pid'] = int(m.groups()[0])
         else:
             return None
+
+        info_dict['connections'] = []
+        topic_list = NodeInfo.topic_re.findall(info_str)
+        lines = info_str.split('\n')
+        topic = None
+        for line in lines:
+            if topic is None:
+                m = NodeInfo.topic_re.search(line)
+                if m:
+                    # start of a topic section
+                    topic = m.groups()[0]
+            else:
+                # already inside a topic section
+                m = NodeInfo.topic_to_re.search(line)
+                if m:
+                    dest_node, uri = m.groups()
+                    info_dict['connections'].append({'topic':topic, 'other_node':dest_node, 'uri':uri})
+        return info_dict
+            
 
     def get_nodes(self):
         '''
         Returns a dictionary whose keys are the name of each running ROS node. The values
         are dictionaries with the following entries:
-
-        pid - (int) process ID of the node
         '''
         node_info_list = {}
         for node_name in self.get_node_names():
---
+++ src/ros_monitor/process_info.py
@@ -0,0 +1,42 @@
+import subprocess
+
+class ProcessInfo:
+   def __init__(self):
+      pass
+
+   def get_stats(self, pids):
+      '''
+      Get stats as a dictionary for the given process.
+      Thin wrapper around "ps exo pid,pcpu,vsz".
+
+      pid - (int) process id of the process.
+      '''
+
+      # these correspond to field names that the command ps understands:
+      # pid - process id
+      # pcpu - percent cpu usage of process
+      # vsz - virtual memory used by process
+      fieldnames = ['pid', 'pcpu', 'vsz']
+      
+      ps_str = subprocess.check_output(['ps', 'exo', ','.join(fieldnames)])
+      ps_lines = ps_str.split('\n')
+      stats = {}
+      for line in ps_lines[1:]:
+          fields = line.strip().split()
+          if len(fields) != len(fieldnames):
+              continue
+
+          field_dict = {}
+          for f_i, fieldname in enumerate(fieldnames):
+              field_str = fields[f_i]
+              if fieldname in ['pid', 'vsz']:
+                  fieldval = int(field_str)
+              elif fieldname in ['pcpu']:
+                  fieldval = float(field_str)
+              else:
+                  fieldval = field_str
+              field_dict[fieldname] = fieldval
+
+          if field_dict['pid'] in pids:
+              stats[field_dict['pid']] = field_dict
+      return stats
