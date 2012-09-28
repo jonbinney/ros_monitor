ceead483b2cb0a50b433fd7f9face6cfacd2e136
root <root@prn1.(none)>
2012-09-27 19:59:33 -0700

    improved error checking and unified launchfile

--- src/ros_monitor/node_info.py
+++ src/ros_monitor/node_info.py
@@ -1,8 +1,9 @@
+import re, urlparse
 import rosnode
 import xmlrpclib
 import rosgraph
 import rospy
-import re
+
 
 class NodeInfo:
     '''
@@ -20,43 +21,25 @@ class NodeInfo:
         return rosnode.get_node_names()
 
     def get_node_info(self, node_name):
-        '''
-        Uses command "rosnode info <node_name>" to get pid and connection info for this node.
-
-        Parsing the command line output for this info seems a bit hacky, but I\'d rather
-        not rely on the internal python API. -jbinney
-        '''
-        
         node_api_uri = rosnode.get_api_uri(self.master, node_name)
         node = xmlrpclib.ServerProxy(node_api_uri)
-        pid = node.getPid(rospy.get_name())
-        
+        code, status_message, pid = node.getPid(rospy.get_name())
 
-        info_dict['connections'] = []
-        topic_list = NodeInfo.topic_re.findall(info_str)
-        lines = info_str.split('\n')
-        topic = None
-        for line in lines:
-            if topic is None:
-                m = NodeInfo.topic_re.search(line)
-                if m:
-                    # start of a topic section
-                    topic = m.groups()[0]
-            else:
-                # already inside a topic section
-                m = NodeInfo.topic_to_re.search(line)
-                if m:
-                    dest_node, uri = m.groups()
-                    info_dict['connections'].append({'topic':topic, 'other_node':dest_node, 'uri':uri})
-        return info_dict
-            
+        netloc = urlparse.urlparse(node_api_uri).netloc
+        host, api_port = netloc.split(':')
+
+        return {'pid': pid, 'host':host}
 
     def get_nodes(self):
         '''
         Returns a dictionary whose keys are the name of each running ROS node. The values
         are dictionaries with the following entries:
         '''
-        node_info_list = {}
+        nodes = {}
         for node_name in self.get_node_names():
-            node_info_list[node_name] = self.get_node_info(node_name)
-        return node_info_list
+            try:
+                nodes[node_name] = self.get_node_info(node_name)
+            except:
+                rospy.logwarn(
+                    'NodeInfo.get_nodes: Unable to get node info for node: %s' % node_name)
+        return nodes
--- src/ros_monitor/process_info.py
+++ src/ros_monitor/process_info.py
@@ -2,40 +2,53 @@ import subprocess
 
 class ProcessInfo:
    def __init__(self):
-      pass
+      self._ps_dict = {}
 
-   def get_stats(self):
+   def get_stats(self, pid, host='localhost'):
       '''
-      Get stats as a dictionary for the given process.
-      Thin wrapper around "ps -A exo pid,pcpu,vsz".
+      Get process info for the given pid running on the given host.
 
-      pid - (int) process id of the process.
+      Call update() first.
+
+      Args:
+          pid (int) - PID to lookup.
+          host (str) - Host that PID is running on.
+
+      Returns:
+          {'pcpu': ___, 'vsz': __,}
       '''
+      return self._ps_dict[(host, pid)]
 
+   def update(self, hosts=['localhost']):
+      '''
+      Thin wrapper around "ssh <host> ps -A exo pid,pcpu,vsz".
+
+      hosts - will ssh into each listed host and run ps. For instance on the pr2, we want to
+      check c1 and c2.
+      '''
       # these correspond to field names that the command ps understands:
       # pid - process id
       # pcpu - percent cpu usage of process
       # vsz - virtual memory used by process
       fieldnames = ['pid', 'pcpu', 'vsz']
-      
-      ps_str = subprocess.check_output(['ps', '-A', 'exo', ','.join(fieldnames)])
-      ps_lines = ps_str.split('\n')
-      stats = {}
-      for line in ps_lines[1:]:
-          fields = line.strip().split()
-          if len(fields) != len(fieldnames):
-              continue
 
-          field_dict = {}
-          for f_i, fieldname in enumerate(fieldnames):
-              field_str = fields[f_i]
-              if fieldname in ['pid', 'vsz']:
-                  fieldval = int(field_str)
-              elif fieldname in ['pcpu']:
-                  fieldval = float(field_str)
-              else:
-                  fieldval = field_str
-              field_dict[fieldname] = fieldval
+      # keys are (host_name, pid), values are {'pcpu': __, 'vsz': __}
+      self._ps_dict = {}
 
-          stats[field_dict['pid']] = field_dict
-      return stats
+      for host in hosts:
+         # starting in python 2.7, we can use subprocess.check_output which is simpler
+         # than Popen. but for now...
+
+         cmd = ['ssh', host, 'ps', '-A', 'exo', ','.join(fieldnames)]
+         ps_str = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+         ps_lines = ps_str.split('\n')
+         for line in ps_lines[1:]:
+            fields = line.strip().split()
+            if len(fields) == 0:
+               continue
+            pid, pcpu, vsz = fields
+            pid = int(pid)
+            pcpu = float(pcpu)
+            vsz = int(vsz)
+
+            self._ps_dict[(host, pid)] = {'pcpu': pcpu, 'vsz': vsz}
